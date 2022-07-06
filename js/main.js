9b4769de811a3f184d06b4c87a4df102f72a33be
saidmoussadek <saiddus24@gmail.com>
2022-07-06 21:43:50 +0200

    push fine

--- js/main.js
+++ js/main.js
@@ -2,8 +2,39 @@ const containerDom=document.getElementById("container")
 
 for (let  i=0;  i <=1000; i++){
     
-    containerDom.innerHTML += `<div class="box">${i}</div>`
+    containerDom.innerHTML += `<div class="box">${i}</div>`;
 
-    console.log (i)   
+    let box =document.querySelector('.box:last-child');
+
+    if ( i % 3 == 0){
+        
+        box.classList.add('red');
+        box.innerHTML = 'buzz';
+
+    }
+    else if( i % 5 == 0){
+
+        box.classList.add('yellow');
+        box.innerHTML = 'fuzz';
+
+        
+
+    }
+    /*else if( i % 5 == 0 )+( i % 3 == 0 ){ */
+           /*( i % 5 == 0  i % 3 == 0 )*/
+           /*( i % 5 == 0 + i % 3 == 0 )*/
+           /*(i % 5 == 0  && i % 3 == 0 )*/
+           /*(i % 5 == 0) && ( i % 3 == 0 )*/
+           /*((i % 5 == 0 ) &&  ( i % 3 == 0))*/
+
+        
+    else if( i % 5 == 0   &&  i % 3 == 0 ){
+        box.classList.add('green');
+        box.innerHTML = 'fuzzbuzz';
+
+        console.log(i);   
+    }
+    
+
 }
 
